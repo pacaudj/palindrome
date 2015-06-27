205994850007264f853a6557020115838fddf03e
pacaud_j <jean-baptiste.pacaud-paris@epitech.eu>
2015-06-27 13:59:14 +0200

    licorne

--- include/palin.h
+++ include/palin.h
@@ -24,6 +24,7 @@ void		my_error_putchar(char);
 void		my_error_putstr(char *);
 void		init_value(int *, int *);
 void		verif_solu(int, char **);
+void		my_rev_init(int *, int *, int *);
 int 		my_ver_palin(char **, char **);
 int 		my_init(char **);
 int 		my_palin(char **, char **);
--- src/rev_palin.c
+++ src/rev_palin.c
@@ -16,14 +16,14 @@ int 			my_rev_palin(char **arg, char **param)
   int 			j;
   char 			*pal;
   char 			*base;
+  int 			min;
 
-  i = 1;
-  pal = NULL;
+  my_rev_init(&i, &min, &j);
+  (param[2] != NULL) ? min = get_nb(param[2]) : 0;
   base = fill_base(atoi(arg[2]));
-  if (is_palin(arg[1]) != 1)
-    my_error_putstr("argument invalide\n");
+  (is_palin(arg[1]) != 1) ? my_error_putstr("argument invalide\n") : 0;
   pal = my_catch_nbrbase(i, base);
-  while (strcmp(pal, arg[1]) != 0)
+  while (strcmp(pal, arg[1]) != 0 || (j > min && min != -1))
     {
       j = 0;
       pal = my_catch_nbrbase(i, base);
@@ -36,4 +36,11 @@ int 			my_rev_palin(char **arg, char **param)
     }
   printf("%d donne %d en %d itération(s) (en base %s)\n",
 	 (i - 1), my_getnbr_base(pal, base), j, arg[2]);
+}
+
+void			my_rev_init(int *a, int *b, int *c)
+{
+	*a = 1;
+  	*b = -1;
+  	*c = 0;
 }
